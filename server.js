d9b08bfe845e75ed673b410563ebad24882467f1
rahish-nyx <rahishraza053@gmail.com>
2026-05-31 16:53:37 +0530

    Force Gmail SMTP IPv4

--- server.js
+++ server.js
@@ -62,6 +62,10 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
+  family: 4,
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
   auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
   tls: { rejectUnauthorized: false },
 });
